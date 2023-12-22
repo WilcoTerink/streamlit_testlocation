19677343c9bd8d124ce1c051e85484aaf431cf67
WilcoTerink <terinkw@gmail.com>
2023-12-22 15:19:24 +1300

    Toevoegingen beregening actief en locatie gebruiker

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import psycopg2
 import geoalchemy2
 import time
 from streamlit_js_eval import streamlit_js_eval
+import haversine as hs
 
 # https://folium.streamlit.app/draw_support
 
@@ -38,7 +39,6 @@ with col1:
     st.text(f"Datum: {pd.Timestamp('now').date().strftime('%d %B %Y')}")
     st.text("Deze app werkt het beste\nin landscape mode")
 
-    
     st.markdown("###### Doorloop onderstaande stappen:")
     st.markdown("###### 1. Bepaal je locatie", help="Check je privacy-instellingen in je telefoon om locatiebepaling toe te staan")
 
@@ -54,80 +54,91 @@ with col1:
             Draw(export=False, draw_options={"polyline": False, "polygon": False, "rectangle": False, "circle": False, "circlemarker": False,
             "marker": {"repeatMode": False}}).add_to(m)
             icon = folium.Icon(color="green", icon="crosshairs", prefix="fa")
-            myLocation = folium.Marker([lat, lon], tooltip="mijn locatie", popup="jojo", icon=icon).add_to(m)
+            myLocation = folium.Marker([lat, lon], tooltip="mijn locatie", icon=icon).add_to(m)
             
             st.caption(f"Locatie nauwkeurigheid is {location['accuracy']} m")
-            st_data = st_folium(m, width=725, center=[lat, lon], zoom=16)
+            st_data = st_folium(m, width=725, center=[lat, lon], zoom=30)
 
         # Selecteer/ markeer punt voor waarneming
-        st.markdown('###### 2. Markeer maximaal 1 perceel op de kaart voor waarneming')
+        st.markdown('###### 2. Markeer maximaal 1 perceel op de kaart voor waarneming', help="Klik hiervoor eerst de 'marker' knop op de kaart onder de zoom knoppen")
         try:
             nPoints = len(st_data["all_drawings"])
             markedLocation = st_data["all_drawings"][nPoints-1]["geometry"]["coordinates"]
+            # Distance check tussen gebruiken en gemarkeerd perceel
+            dist = hs.haversine((lat, lon), (markedLocation[1], markedLocation[0])) * 1000
         except:
             markedLocation = False
-            #nPoints = 0
         
-        #if (nPoints == 1) and markedLocation:
         if markedLocation:
             if nPoints == 1:
-            
-            # Laat rest van formulier zien zodra waarneming is gemarkeerd op kaart
-            #if markedLocation:
-                loc_lon = markedLocation[0]
-                loc_lat = markedLocation[1]
-                st.text((loc_lat, loc_lon))
+                if dist <= 100:
+                    # Laat rest van formulier zien zodra waarneming is gemarkeerd op kaart en voldoet aan de eisen.
+                    loc_lon = markedLocation[0]
+                    loc_lat = markedLocation[1]
+                    st.text((loc_lat, loc_lon))
 
-                st.markdown('###### 3. Beantwoord de volgende vragen')
+                    st.markdown('###### 3. Beantwoord de volgende vragen')
 
-                gewassen = ['kale grond', 'gras', 'mais', 'aardappel', 'suikerbieten', 'weet ik niet']
-                landgebruik = st.selectbox('Landgebruik/gewas?', gewassen, placeholder='weet ik niet', index=len(gewassen)-1)
+                    gewassen = ['kale grond', 'gras', 'mais', 'aardappel', 'suikerbieten', 'weet ik niet']
+                    landgebruik = st.selectbox('Landgebruik/gewas?', gewassen, placeholder='weet ik niet', index=len(gewassen)-1)
 
-                beregening = st.toggle('Perceel beregend?')
-                if beregening:
-                    bron = st.selectbox(
-                        'Wat is de bron van beregening?',
-                        ['grondwater', 'oppervlaktewater', 'weet ik niet'], placeholder='weet ik niet', index=2)
-                        #['grondwater', 'oppervlaktewater', 'weet ik niet'], index=None, placeholder='maak keuze')
+                    beregening = st.toggle('Haspel en/of sproeier aanwezig op perceel?')
+                    if beregening:
+                        
+                        beregening_actief = st.toggle('Staat de beregening nu aan?')
 
-                opmerking = st.text_area("Opmerkingen:")
+                        bron = st.selectbox(
+                            'Wat is de bron van beregening?',
+                            ['grondwater', 'oppervlaktewater', 'weet ik niet'], placeholder='weet ik niet', index=2)
+                    else:
+                        beregening_actief = False
 
-                submit = st.button('Upload waarneming')
-                if submit:
-                    with st.spinner('Moment a.u.b. Uw waarneming wordt ge-upload...'):
-                        # Initialize connection.
-                        engine = initDB()
+                    opmerking = st.text_area("Opmerkingen:")
 
-                        if engine:
-                            # Create new db entry
-                            df_new = pd.DataFrame(columns=['id', 'geometry', 'lon', 'lat', 'landgebruik', 'beregend', 'opmerking', 'bron'])
-                            df_new.loc[0, 'lon'] = loc_lon
-                            df_new.loc[0, 'lat'] = loc_lat
-                            df_new.loc[0, 'geometry'] = gpd.points_from_xy(df_new.lon, df_new.lat, crs='EPSG:4326')[0]
-                            df_new.drop(['lon', 'lat'], axis=1, inplace=True)
-                            if landgebruik != 'weet ik niet':
-                                df_new.loc[0, 'landgebruik'] = landgebruik
-                            df_new.loc[0, 'beregend'] = beregening
-                            if beregening and (bron != 'weet ik niet'):
-                                df_new.loc[0, 'bron'] = bron
-                            if len(opmerking) > 0:
-                                df_new.loc[0, 'opmerking'] = opmerking
-                            df_new['timestamp'] = pd.Timestamp('now')
-                            df_new = gpd.GeoDataFrame(df_new, geometry='geometry', crs='EPSG:4326')
+                    submit = st.button('Upload waarneming')
+                    if submit:
+                        with st.spinner('Moment a.u.b. Uw waarneming wordt ge-upload...'):
+                            # Initialize connection.
+                            engine = initDB()
 
-                            # Get latest entry from db
-                            df_latest = pd.read_sql('select * from streamlit_test.crowd_beregening order by timestamp desc limit 3', con=engine)
-                            latest_id = 1
-                            if len(df_latest) > 0:
-                                latest_id = int(df_latest.id.iloc[0])
-                                if latest_id is not None:
-                                    latest_id += 1
-                            df_new.loc[0, 'id'] = latest_id
-                            df_new.to_postgis('crowd_beregening', con=engine, schema='streamlit_test', if_exists="append")
-                    
-                    st.success('Waarneming is ge-upload naar het beregeningsportaal. We danken u voor uw medewerking. App wordt binnen enkele seconden herladen.')
-                    time.sleep(5)
-                    streamlit_js_eval(js_expressions="parent.window.location.reload()")
+                            if engine:
+                                # Create new db entry
+                                df_new = pd.DataFrame(columns=['id', 'lon', 'lat', 'landgebruik', 'beregening', 'beregening_actief', 'bron', 'opmerking', 'geometry_perceel', 'geometry_user'])
+                                df_new.loc[0, 'lon'] = loc_lon
+                                df_new.loc[0, 'lat'] = loc_lat
+                                df_new.loc[0, 'geometry_perceel'] = gpd.points_from_xy(df_new.lon, df_new.lat, crs='EPSG:4326')[0]
+                                if landgebruik != 'weet ik niet':
+                                    df_new.loc[0, 'landgebruik'] = landgebruik
+                                df_new.loc[0, 'beregening'] = beregening
+                                df_new.loc[0, 'beregening_actief'] = beregening_actief
+                                if beregening and (bron != 'weet ik niet'):
+                                    df_new.loc[0, 'bron'] = bron
+                                if len(opmerking) > 0:
+                                    df_new.loc[0, 'opmerking'] = opmerking
+                                df_new.loc[0, 'lon'] = lon
+                                df_new.loc[0, 'lat'] = lat
+                                df_new.loc[0, 'geometry_user'] = gpd.points_from_xy(df_new.lon, df_new.lat, crs='EPSG:4326')[0]
+                                df_new.drop(['lon', 'lat'], axis=1, inplace=True)
+                                # UTC timestamp
+                                df_new['timestamp'] = pd.Timestamp.utcnow()     #pd.Timestamp('now')
+                                df_new = gpd.GeoDataFrame(df_new, geometry='geometry_perceel', crs='EPSG:4326')
+                                st.write(df_new)
+
+                                # # Get latest entry from db
+                                # df_latest = pd.read_sql('select * from streamlit_test.crowd_beregening order by timestamp desc limit 3', con=engine)
+                                # latest_id = 1
+                                # if len(df_latest) > 0:
+                                #     latest_id = int(df_latest.id.iloc[0])
+                                #     if latest_id is not None:
+                                #         latest_id += 1
+                                # df_new.loc[0, 'id'] = latest_id
+                                # df_new.to_postgis('crowd_beregening', con=engine, schema='streamlit_test', if_exists="append")
+                        
+                        st.success('Waarneming is ge-upload naar het beregeningsportaal. We danken u voor uw medewerking. App wordt binnen enkele seconden herladen.')
+                        time.sleep(5)
+                        #streamlit_js_eval(js_expressions="parent.window.location.reload()")
+                else:
+                    st.markdown('###### :red[Selecteer een perceel dichtbij je locatie]')
             else:
                 st.markdown('###### :red[Markeer maximaal 1 locatie]')
 
